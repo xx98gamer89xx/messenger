173726c92b971042420dc960abee0f9fe6551f59
Xxgamer89xX <donarbustin123@gmail.com>
2025-10-20 00:12:45 +0200

    Se añadieron los archivos server.py y client.py version 0.1 (prueba funcional pero básica)

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+messages = {}
+
+@app.route("/send_message", methods=["POST"])
+def send_message():
+    data = request.json
+    receiver = data['to']
+    sender = data["from"]
+    message = data['message']
+    messages.setdefault(receiver, []).append([message, sender])
+    return jsonify({"status": "ok"})
+@app.route("/receive_messages", methods=["GET"])
+def receive_messages():
+    user = request.args.get('user')
+    msgs = messages.get(user, [])
+    messages[user] = []
+    return jsonify(msgs)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
